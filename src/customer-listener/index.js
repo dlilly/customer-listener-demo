a027ae065c51bbaf80a6a0a1c2fc47deabd2ebb5
Dave Lilly <dave.lilly@commercetools.de>
2020-10-21 14:29:25 -0500

    Consolidation

--- src/customer-listener/index.js
+++ src/customer-listener/index.js
@@ -1,17 +1,14 @@
-let updated = ({ message, resource }) => {
-    console.log(`\nCustomer Updated`)
-    console.log(`----------------\n`)
-    console.log(`Message: ${JSON.stringify(message, '', 4)}\n`)
-    console.log(`Customer: ${JSON.stringify(resource, '', 4)}`)
-}
-
 module.exports = {
     subscriptions: [{
         key: 'customer-listener',
         changes: {
             customer: {
-                ResourceCreated: updated,
-                ResourceUpdated: updated
+                ResourceUpdated: ({ message, resource }) => {
+                    console.log(`\nCustomer Updated`)
+                    console.log(`----------------\n`)
+                    console.log(`Message: ${JSON.stringify(message, '', 4)}\n`)
+                    console.log(`Customer: ${JSON.stringify(resource, '', 4)}`)
+                }
             }
         }
     }]
